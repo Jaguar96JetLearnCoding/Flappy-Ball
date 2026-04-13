dbddf7f09466a922f7d608dda31f8d918476e5b4
Jaguar96JetLearnCoding <luiza_popovici@yahoo.co.uk>
2026-04-13 12:10:16 +0100

    Add files via upload

---
+++ Flappy Ball.py
@@ -0,0 +1,32 @@
+import pgzrun
+import random
+WIDTH=600
+HEIGHT=600
+a=1000
+#Class for the ball
+class Ball():
+    def __init__(self,radius,x,y,colour):
+        self.radius=radius
+        self.x=x
+        self.y=y
+        self.vx=20
+        self.vy=0
+        self.colour=colour
+    def draw(self):
+        screen.draw.filled_circle((self.x,self.y),self.radius,self.colour)
+red_ball=Ball(20,300,300,"Red")
+        
+def draw():
+    screen.fill("black")
+    red_ball.draw()
+def update(dt):
+    uy=red_ball.vy
+    red_ball.vy+=a*dt
+    s=(uy+red_ball.vy)/2*dt
+    red_ball.y+=s
+
+    if red_ball.y>600-red_ball.radius:
+        red_ball.vy*=-0.85
+        red_ball.y=600-red_ball.radius
+pgzrun.go()
+
